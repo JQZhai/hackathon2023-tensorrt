64e4ea35066b6249656962eb95da664e3fc3f2ab
JQZhai <43233371+JQZhai@users.noreply.github.com>
2023-08-08 17:45:09 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -10,10 +10,10 @@ trtexec \
 
 trtexec \
 --onnx=./clip.onnx \
---optShapes=input_ids:1x77 \
 --workspace=10240 \
 --saveEngine=./clip.plan \
 --skipInference  
+# --optShapes=input_ids:1x77 \
 
 trtexec \
 --onnx=./vae.onnx \
@@ -24,5 +24,6 @@ trtexec \
 
 
 trtexec \
---onnx=./unet.onnx --saveEngine=unet.plan  --skipInference --fp16 #\
+--onnx=./unet.onnx --saveEngine=unet.plan  --skipInference --fp16 
+#\
 # --optShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6
