e119d938dbe8f3e17fcc57fc8330f922ecbdc01f
JQZhai <43233371+JQZhai@users.noreply.github.com>
2023-08-09 20:55:43 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -3,10 +3,10 @@ python3 2onnx.py
 
 trtexec \
 --onnx=./controlnet.onnx \
---optShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768 \
 --workspace=10240 \
 --saveEngine=./controlnet.plan \
 --fp16 --skipInference 
+# --optShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768 \
 
 trtexec \
 --onnx=./clip.onnx \
@@ -17,10 +17,10 @@ trtexec \
 
 trtexec \
 --onnx=./vae.onnx \
---optShapes=latent:1x4x32x48 \
 --workspace=10240 \
 --saveEngine=./vae.plan \
 --skipInference 
+# --optShapes=latent:1x4x32x48 \
 
 
 trtexec \
