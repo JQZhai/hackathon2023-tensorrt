ed92212e0e9c713136c0953f3077c1fd82f03309
JQZhai <43233371+JQZhai@users.noreply.github.com>
2023-08-10 18:26:45 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -5,10 +5,10 @@ trtexec \
 --onnx=./controlnet.onnx \
 --saveEngine=./controlnet.plan \
 --fp16 \
---builderOptimizationLevel=5 \
 --optShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768 \
 --minShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768 \
 --maxShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768
+# --builderOptimizationLevel=5 \
 
 trtexec \
 --onnx=./clip.onnx \
