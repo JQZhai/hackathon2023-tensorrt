67869a12cc8f6745fcc29677a77d2fe50cca316d
JQZhai <43233371+JQZhai@users.noreply.github.com>
2023-08-11 00:00:16 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -4,11 +4,10 @@ python3 2onnx.py
 trtexec \
 --onnx=./controlnet.onnx \
 --saveEngine=./controlnet.plan \
---fp16 \
 --optShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768 \
 --minShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768 \
 --maxShapes=x:1x4x32x48,hint:1x3x256x384,timesteps:1,context:1x77x768
-# --builderOptimizationLevel=5 \
+# --builderOptimizationLevel=5 \ --fp16 \
 
 trtexec \
 --onnx=./clip.onnx \
@@ -24,12 +23,12 @@ trtexec \
 --builderOptimizationLevel=5 \
 --optShapes=latent:1x4x32x48 \
 --minShapes=latent:1x4x32x48 \
---maxShapes=latent:1x4x32x48
+--maxShapes=latent:1x4x32x48 --fp16
 
 
 trtexec \
 --onnx=./unet.onnx --saveEngine=unet.plan  \
---builderOptimizationLevel=5  \
+--builderOptimizationLevel=5  --fp16 \
 --optShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6 \
 --minShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6 \
 --maxShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6
