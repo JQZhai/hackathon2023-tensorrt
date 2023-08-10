3d9b03ec15d4a5678fae46be68e9507ccd53f94f
JQZhai <43233371+JQZhai@users.noreply.github.com>
2023-08-10 22:14:51 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -24,12 +24,12 @@ trtexec \
 --builderOptimizationLevel=5 \
 --optShapes=latent:1x4x32x48 \
 --minShapes=latent:1x4x32x48 \
---maxShapes=latent:1x4x32x48 --fp16
+--maxShapes=latent:1x4x32x48
 
 
 trtexec \
 --onnx=./unet.onnx --saveEngine=unet.plan  \
---builderOptimizationLevel=5 --fp16 \
+--builderOptimizationLevel=5  \
 --optShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6 \
 --minShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6 \
 --maxShapes=x:1x4x32x48,timesteps:1,context:1x77x768,control_0:1x320x32x48,control_1:1x320x32x48,control_2:1x320x32x48,control_3:1x320x16x24,control_4:1x640x16x24,control_5:1x640x16x24,control_6:1x640x8x12,control_7:1x1280x8x12,control_8:1x1280x8x12,control_9:1x1280x4x6,control_10:1x1280x4x6,control_11:1x1280x4x6,control_12:1x1280x4x6
